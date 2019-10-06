19643605107e53cd832da606f1adb0d48f158e8b
gaurav <gauravgola96@gmail.com>
2019-10-06 12:39:12 +0530

    config files

---
+++ config.py
@@ -0,0 +1,22 @@
+mlsql_api = 'http://XXXXXX'
+
+
+
+
+# import os
+# # print(os.curdir)
+# # print(os.listdir('./'))
+# path = 'ticket_sample.csv'
+#
+# csv = {'csv': open(path, 'rb').read()}
+
+# what is the description_text of id 363004
+
+
+# what is count of bag with status as out for pickup
+
+# what is the count of number of bag id date after  21-07-2016
+# what is the count of bag id with status as out for pickup and state id as 4
+# what is the count of number of bag id after 21-07-2016
+
+
